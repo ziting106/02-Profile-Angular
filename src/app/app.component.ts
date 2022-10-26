f546bfa7e12b991bb39bf166eaa9e8f1987b2a3e
Angelica Liu <ange83106@gmail.com>
2022-10-26 20:05:29 +0800

    start from hear~

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Injectable } from '@angular/core';
 
+@Injectable()
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = '02-Profile-Angular';
+  constructor() {}
 }
